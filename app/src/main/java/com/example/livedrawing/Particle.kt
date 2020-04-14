fbfe403630800a8fbfa87477ca1be64ae524b82b
Duke-Stark <chaharchandresh@gmail.com>
2020-04-14 22:36:41 +0530

    coding the particleSystem class to create instances of Particle class

--- app/src/main/java/com/example/livedrawing/Particle.kt
+++ app/src/main/java/com/example/livedrawing/Particle.kt
@@ -1,4 +1,20 @@
 package com.example.livedrawing
 
-class Particle {
+import android.graphics.PointF
+
+
+class Particle(direction : PointF) {
+
+    private val velocity : PointF = PointF()
+    val position : PointF = PointF()
+
+    init {
+        velocity.x = direction.x
+        velocity.y = direction .y
+    }
+
+    fun update(){
+        position.x+= velocity.x
+        position.y += velocity.y
+    }
 }
--- app/src/main/java/com/example/livedrawing/ParticleSystem.kt
+++ app/src/main/java/com/example/livedrawing/ParticleSystem.kt
@@ -1,4 +1,36 @@
 package com.example.livedrawing
 
+import android.graphics.PointF
+import java.util.*
+import kotlin.collections.ArrayList
+
 class ParticleSystem {
+
+    private var duration : Float = 0f
+    private var particles : ArrayList<Particle> = ArrayList()
+
+    private val random = Random()
+    var isRunning = false
+
+    fun initParticles(numParticles : Int){
+
+        for ( i in 0 until numParticles){
+
+            var angle : Double = random.nextInt(360).toDouble()
+            angle *= (3.14 / 180)
+
+            //val speed = random.nextFloat() / 3
+
+            val speed = (random.nextInt(10) + 1);
+
+            val direction : PointF
+            direction = PointF(Math.cos(
+                angle).toFloat() * speed ,
+                Math.sin(angle).toFloat() * speed
+            )
+
+            particles. add ( Particle(direction))
+
+        }
+    }
 }
